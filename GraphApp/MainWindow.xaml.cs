90bbcc38ccf7f3d2aa4ea33901fa6de30f7390ce
Marcin filapek <filap4@gmail.com>
2019-11-03 22:27:14 +0100

    upgrade

--- GraphApp/MainWindow.xaml.cs
+++ GraphApp/MainWindow.xaml.cs
@@ -23,12 +23,11 @@ namespace GraphApp
         private int[][] GraphArray { get; set; }
         private double xPosition;
         private double yPosition;
-        private Model3DGroup MainModel3Dgroup = new Model3DGroup();
+
 
         public MainWindow()
         {
             InitializeComponent();
-
         }
 
         #region Window state
@@ -111,19 +110,25 @@ namespace GraphApp
 
         #region Button 
 
-        private void ClearGraphBtn_Click(object sender, RoutedEventArgs e) => SpaceToDraw.Children.Clear();
+        private void ClearGraphBtn_Click(object sender, RoutedEventArgs e) => Viewport3D.Children.Clear();
 
         private async void DrawGraphBtn_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            if (!await TheCorrectnessOfData.IsAValueInTheRange(10, 30, TextBoxNodes.Text) || !await TheCorrectnessOfData.IsAValueInTheRange(0.35, 0.45, TextBoxPropability.Text) ||
-                !await TheCorrectnessOfData.IsTheCorrectNumberOfEdges(TextBoxEdges.Text, TextBoxNodes.Text))
+            if (!await TheCorrectnessOfData.IsAValueInTheRange(10, 30, TextBoxNodes.Text) || !await TheCorrectnessOfData.IsAValueInTheRange(0.35, 0.45, TextBoxPropability.Text))
             {
                 MessageBox.Show("Incorect value", "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
                 TextBoxNodes.Clear();
+                TextBoxPropability.Clear();
+                return;
+            }
+            if (await TheCorrectnessOfData.IsTheCorrectNumberOfEdges(TextBoxEdges.Text, TextBoxNodes.Text))
+            {
+                MessageBox.Show("Value out of the range", "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
+                TextBoxEdges.Clear();
                 return;
             }
 
-            if (TextBoxNodes.Text == "")
+            if (TextBoxEdges.Text == "")
             {
                 GraphArray = await ArrayCreator.GenerateGraphArray(int.Parse(TextBoxNodes.Text), double.Parse(TextBoxPropability.Text));
                 DrawGraph(GraphArray);
@@ -133,6 +138,8 @@ namespace GraphApp
                 GraphArray = await ArrayCreator.GenerateGraphArray(int.Parse(TextBoxNodes.Text), double.Parse(TextBoxPropability.Text), int.Parse(TextBoxEdges.Text));
                 DrawGraph(GraphArray);
             }
+
+
         }
 
         #endregion
@@ -144,7 +151,11 @@ namespace GraphApp
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        private async void TextBoxNodes_PreviewTextInput(object sender, TextCompositionEventArgs e) => e.Handled = !await TheCorrectnessOfData.IsTextAllowed(e.Text);
+        private async void TextBoxNodes_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !await TheCorrectnessOfData.IsTextAllowed(e.Text);
+        }
+
         /// <summary>
         /// Check text before input
         /// </summary>
@@ -202,7 +213,7 @@ namespace GraphApp
         private async void SaveCommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
         {
             var filter = "PDF (*.pdf)|*.pdf";
-           // await Class.File.SaveFile(NodeCord, GraphArray, filter);
+            // await Class.File.SaveFile(NodeCord, GraphArray, filter);
         }
         private async void SaveAsCommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
         {
@@ -243,52 +254,21 @@ namespace GraphApp
 
             for (int i = 0; i < array.Length; i++)
             {
-                DefineModel(MainModel3Dgroup, NodeCord[i]);
+                Model3DGroup model3DGroup = new Model3DGroup();
+                DefineModel(model3DGroup, NodeCord[i]);
                 ModelVisual3D modelVisual = new ModelVisual3D();
-                modelVisual.Content = MainModel3Dgroup;
+                modelVisual.Content = model3DGroup;
 
                 Viewport3D.Children.Add(modelVisual);
             }
+
         }
 
         #endregion
 
-        private void SaveFile(string filter)
-        {
-            SaveFileDialog saveFileDialog = new SaveFileDialog();
-            saveFileDialog.Filter = filter;
-            saveFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-            if (saveFileDialog.ShowDialog() == true)
-            {
-                PdfDocument doc = new PdfDocument();
-                PdfPageBase page = doc.Pages.Add();
-                page.Canvas.DrawEllipse(PdfBrushes.Black, 100, 100, 20, 20);
-                page.Canvas.DrawEllipse(PdfBrushes.Black, 200, 200, 20, 20);
-                page.Canvas.DrawLine(PdfPens.Blue, 110, 110, 210, 210);
-                page.Canvas.DrawString("A",
-                            new PdfFont(PdfFontFamily.Helvetica, 10),
-                            new PdfSolidBrush(new PdfRGBColor(255, 255, 255)),
-                            105, 105);
-
-                page.Canvas.DrawLine(PdfPens.Black, 10, 740, 585, 740);
-                page.Canvas.DrawString(DateTime.UtcNow.ToString(),
-                           new PdfFont(PdfFontFamily.Helvetica, 14),
-                           new PdfSolidBrush(new PdfRGBColor(0, 0, 0)),
-                           330, 740);
-
-                var path = saveFileDialog.FileName;
-                FileStream stream = System.IO.File.Create(path);
-                stream.Close();
-                FileStream toStream = new FileStream(path, FileMode.Open);
-                doc.SaveToStream(toStream);
-                toStream.Close();
-                doc.Close();
-            }
-        }
-
         #region Viewport3D
 
-        private void SpaceToDraw_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        private void SpaceToDraw_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
         {
             ModelVisual3D obj = null;
             HitTestResult hitTest = VisualTreeHelper.HitTest(SpaceToDraw, e.GetPosition(SpaceToDraw));
@@ -302,25 +282,25 @@ namespace GraphApp
         }
         private void Camera_MouseMove(object sender, MouseEventArgs e)
         {
-            if (e.GetPosition(SpaceToDraw).X > xPosition && e.LeftButton == MouseButtonState.Pressed)
+            if (e.GetPosition(SpaceToDraw).X > xPosition && e.RightButton == MouseButtonState.Pressed)
             {
                 PerspectiveCamera.Position = new Point3D(PerspectiveCamera.Position.X + 0.04, PerspectiveCamera.Position.Y, PerspectiveCamera.Position.Z);
                 xPosition = e.GetPosition(SpaceToDraw).X;
                 yPosition = e.GetPosition(SpaceToDraw).Y;
             }
-            if (e.GetPosition(SpaceToDraw).X < xPosition && e.LeftButton == MouseButtonState.Pressed)
+            if (e.GetPosition(SpaceToDraw).X < xPosition && e.RightButton == MouseButtonState.Pressed)
             {
                 PerspectiveCamera.Position = new Point3D(PerspectiveCamera.Position.X - 0.04, PerspectiveCamera.Position.Y, PerspectiveCamera.Position.Z);
                 xPosition = e.GetPosition(SpaceToDraw).X;
                 yPosition = e.GetPosition(SpaceToDraw).Y;
             }
-            if (e.GetPosition(SpaceToDraw).Y > yPosition && e.LeftButton == MouseButtonState.Pressed)
+            if (e.GetPosition(SpaceToDraw).Y > yPosition && e.RightButton == MouseButtonState.Pressed)
             {
                 PerspectiveCamera.Position = new Point3D(PerspectiveCamera.Position.X, PerspectiveCamera.Position.Y - 0.04, PerspectiveCamera.Position.Z);
                 xPosition = e.GetPosition(SpaceToDraw).X;
                 yPosition = e.GetPosition(SpaceToDraw).Y;
             }
-            if (e.GetPosition(SpaceToDraw).Y < yPosition && e.LeftButton == MouseButtonState.Pressed)
+            if (e.GetPosition(SpaceToDraw).Y < yPosition && e.RightButton == MouseButtonState.Pressed)
             {
                 PerspectiveCamera.Position = new Point3D(PerspectiveCamera.Position.X, PerspectiveCamera.Position.Y + 0.04, PerspectiveCamera.Position.Z);
                 xPosition = e.GetPosition(SpaceToDraw).X;
@@ -339,19 +319,27 @@ namespace GraphApp
         //move to outside class
         private async void DefineModel(Model3DGroup model_group, Point3D centerPoint)
         {
-            MeshGeometry3D mesh1 = new MeshGeometry3D();
-            await Draw.Node(mesh1, centerPoint, 0.05, 10, 50);
-            SolidColorBrush brush1 = Brushes.White;
-            DiffuseMaterial material1 = new DiffuseMaterial(brush1);
-            GeometryModel3D model1 = new GeometryModel3D(mesh1, material1);
-            model_group.Children.Add(model1);
+            MeshGeometry3D mesh = new MeshGeometry3D();
+            await Draw.Node(mesh, centerPoint, 0.05, 30, 100);
+            SolidColorBrush brush = Brushes.White;
+            DiffuseMaterial material = new DiffuseMaterial(brush);
+            GeometryModel3D model = new GeometryModel3D(mesh, material);
+            model_group.Children.Add(new DirectionalLight(Colors.White, new Vector3D(-0.612372, -0.5, -0.612372)));
+            model_group.Children.Add(model);
         }
 
-        private void SpaceToDraw_MouseDown(object sender, MouseButtonEventArgs e)
+        private void SpaceToDraw_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             if (e.LeftButton != MouseButtonState.Pressed)
                 return;
 
+            MouseMove += NodeMove;
+            xPosition = e.GetPosition(SpaceToDraw).X;
+            yPosition = e.GetPosition(SpaceToDraw).Y;
+        }
+
+        private void NodeMove(object sender, MouseEventArgs e)
+        {
             ModelVisual3D obj = null;
             HitTestResult hitTest = VisualTreeHelper.HitTest(SpaceToDraw, e.GetPosition(SpaceToDraw));
 
@@ -361,7 +349,14 @@ namespace GraphApp
             if (obj == null)
                 return;
 
-            obj.Transform = new TranslateTransform3D(-e.GetPosition(SpaceToDraw).X / 1000, -e.GetPosition(SpaceToDraw).Y / 1000, 0);
+            var positionX = (e.GetPosition(SpaceToDraw).X - xPosition) / 1000;
+            var positionY = (yPosition - e.GetPosition(SpaceToDraw).Y) / 1000;
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                obj.Transform = new TranslateTransform3D(positionX, positionY, 0);
+                xPosition = e.GetPosition(SpaceToDraw).X;
+                yPosition = e.GetPosition(SpaceToDraw).Y;
+            }
         }
 
         #endregion
